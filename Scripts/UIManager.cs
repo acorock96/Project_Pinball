c199ee88ccf1abf58f131296919c4e1f81b4eaa0
acorock96 <43269217+acorock96@users.noreply.github.com>
2018-10-01 09:45:57 +0200

    Subo el script del PlayerPref

---
+++ Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public Text scoreText;
+    private int score;
+
+    private void Start()
+    {
+        scoreText.text = PlayerPrefs.GetInt("Score").ToString();
+        score = PlayerPrefs.GetInt("Score");    //Definir el player pref
+    }
+
+    public void ScoreUpdate()
+    {
+        score++;
+        scoreText.text = score.ToString();
+        print("Tienes " + score + " puntos");
+        PlayerPrefs.SetInt("Score", score);     //Actualizar el playerpref con el int
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+        print("Tu puntacion es 0");
+        PlayerPrefs.SetInt("Score", score);     //Actualizar el playerpref con el int
+    }
+}
